f709f389c4e82c145a1a6a38e6626a2f3b255296
syedmaqbolagha <syedmaqbolagha000123@gmail.com>
2025-11-22 20:52:27 +0500

    Added MYproject folder

---
+++ MYproject/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'memory-master-v3';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0/dist/confetti.browser.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});S
